5b2f162fd7266169c34f7f43f7b3ad232797f5ef
Milan Camus <pro@turtletv.fr>
2023-01-18 13:51:25 +0100

    🔧 Use env file for API url

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import router from "./router";
 
 import "ant-design-vue/dist/antd.css";
 import "./assets/main.css";
+import { API_URL } from "./environements";
 
 const app = createApp(App);
 
-axios.defaults.baseURL = "https://localhost";
+axios.defaults.baseURL = API_URL;
 
 app.use(router);
 app.use(Antd).mount("#app");
