48f531a9b2e9af479dcdba7b3192c4b1f6d514ac
Milan Camus <pro@turtletv.fr>
2023-01-18 14:28:33 +0100

    ✨ Implement email confirmation

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import ProfileView from "../views/ProfileView.vue";
 import RegisterView from "../views/RegisterView.vue";
+import ConfirmEmail from "../views/ConfirmEmailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: "/profile",
       name: "profile",
       component: ProfileView,
+    },
+    {
+      path: "/confirm/:token",
+      name: "confirm",
+      component: ConfirmEmail,
     },
   ],
 });
---
+++ src/views/ConfirmEmailView.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- add antd col -->
+  <a-col :span="8" :offset="8">
+    <a-card title="Vérification de l'adresse mail">
+      <a-typography-text>{{ state.message }}</a-typography-text>
+    </a-card>
+  </a-col>
+</template>
+<script>
+import axios from "axios";
+import { defineComponent, reactive } from "vue";
+import router from "../router";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      message: "Votre compte est en cours de vérification...",
+    });
+
+    // log le router actuel
+
+    const token = router.currentRoute.value.params.token;
+    axios
+      .post(`/users/verify/${token}`, {})
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          router.push("/login");
+        }
+      })
+      .catch((err) => {
+        const res = err.response;
+        switch (res.data.message) {
+          case "Invalid token":
+            state.message =
+              "Le token est invalide, veuillez réessayer. Si le problème persiste, veuillez contacter le support.";
+
+            break;
+
+          default:
+            break;
+        }
+
+        console.log(res);
+      });
+
+    return {
+      state,
+    };
+  },
+});
+</script>
