436d9182c155d206dadd7c43b18b45e58e5dd963
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-01-19 20:20:29 +0100

    [ADD] add google-maps

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGeolocation from "vuejs3-google-maps"
+import GMaps from "vuejs3-google-maps";
 
 import "ant-design-vue/dist/antd.css";
 import "./assets/main.css";
-import { API_URL } from "./environements";
+import { API_URL, API_GOOGLE_MAP_KEY } from "./environements";
 import axios from "axios";
 
 const pinia = createPinia();
@@ -14,6 +16,11 @@ const app = createApp(App);
 
 axios.defaults.baseURL = API_URL;
 
+app.use(GMaps, {
+    load: {
+        apiKey: API_GOOGLE_MAP_KEY,
+    },
+});
 app.use(pinia);
 app.use(router);
 app.use(Antd).mount("#app");
