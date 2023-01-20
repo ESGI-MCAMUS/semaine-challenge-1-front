7e398c03b328fb9a7331ea0c93388ef1c9ca6910
Milan Camus <pro@turtletv.fr>
2023-01-20 19:19:05 +0100

    ✨ Implement filters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script>
 import { HeartFilled, HeartOutlined } from "@ant-design/icons-vue";
 import { notification } from "ant-design-vue";
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import Footer from "../components/Footer.vue";
 import router from "../router";
 import { client } from "../services";
 import { formatPrice } from "../utils/ads.utils";
+import { buildQueryString } from "../utils/filters.utils";
 import { isFavorite, refetchFavorites } from "../utils/favorites";
 import { favorites, token } from "../utils/localStorage";
 
@@ -21,18 +22,45 @@ export default defineComponent({
       totalPages: 0,
       totalItems: 0,
       itemPerPage: 0,
+      filtersEnabled: false,
+      checkedList: [],
+      typeOptions: "all",
+      classification: "all",
+      filteredProperties: [],
+      searchText: "",
     });
 
     const getAds = () => {
       client
-        .get(`/real_estate_ads?page=${state.page}`)
+        .get(
+          `/real_estate_ads${
+            state.filtersEnabled
+              ? `?pagination=false${
+                  state.searchText !== "" ? `&title=${state.searchText}` : ""
+                }`
+              : `?page=${state.page}${
+                  state.searchText !== "" ? `&title=${state.searchText}` : ""
+                }`
+          }`
+        )
         .then((res) => {
           const data = res.data;
           state.totalItems = data["hydra:totalItems"];
           state.ads = data["hydra:member"];
           state.itemPerPage = state.ads.length;
-          state.totalPages = Number(data["hydra:view"]["hydra:last"].slice(-1));
+          state.totalPages = !state.filtersEnabled
+            ? Number(data["hydra:view"]["hydra:last"].slice(-1))
+            : -1;
           refetchFavorites();
+          if (state.filtersEnabled) {
+            let filteredAds = state.ads.map((ad) => {
+              const isPresent = state.filteredProperties.some((fp) => {
+                return `/housings/${fp}` == ad.housing;
+              });
+              return isPresent ? ad : undefined;
+            });
+            state.ads = filteredAds.filter((ad) => ad !== undefined);
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -90,6 +118,72 @@ export default defineComponent({
 
     getAds();
 
+    /**
+     * ADVANCED SEARCH
+     */
+    const plainOptions = [
+      "Parking",
+      "Jardin",
+      "Piscine",
+      "Grotte",
+      "Grenier",
+      "Balcon",
+      "Transport en commun",
+    ];
+
+    const handleChangeSelectType = (value) => {
+      state.typeOptions = value;
+      onChangedValues();
+    };
+
+    const handleChangeSelectClassification = (value) => {
+      state.classification = value;
+      onChangedValues();
+    };
+
+    const onChangedValues = () => {
+      const options = {
+        hasParking: state.checkedList.includes("Parking") ? true : undefined,
+        hasGarden: state.checkedList.includes("Jardin") ? true : undefined,
+        hasPool: state.checkedList.includes("Piscine") ? true : undefined,
+        hasCave: state.checkedList.includes("Grotte") ? true : undefined,
+        hasAttic: state.checkedList.includes("Grenier") ? true : undefined,
+        hasBalcony: state.checkedList.includes("Balcon") ? true : undefined,
+        hasPublicTransport: state.checkedList.includes("Transport en commun")
+          ? true
+          : undefined,
+        type: state.typeOptions !== "all" ? state.typeOptions : undefined,
+        classification:
+          state.classification !== "all" ? state.classification : undefined,
+      };
+
+      const query = buildQueryString(options);
+
+      client
+        .get(
+          `/housing_properties?pagination=false${
+            state.filtersEnabled ? query : ""
+          }`
+        )
+        .then((res) => {
+          state.filteredProperties = res.data["hydra:member"].map(
+            (item) => item.id
+          );
+          getAds();
+          if (state.filtersEnabled) {
+            state.totalItems = state.filteredProperties.length;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          notification["error"]({
+            message: "Oups !",
+            description:
+              "Une erreur est survenue lors de l'application des filtres !",
+          });
+        });
+    };
+
     return {
       state,
       getAds,
@@ -99,6 +193,11 @@ export default defineComponent({
       addFavortiteAd,
       removeFavoriteAd,
       isFavorite,
+      plainOptions,
+      onChangedValues,
+      handleChangeSelectType,
+      handleChangeSelectClassification,
+      updateSearchText,
     };
   },
 });
@@ -106,6 +205,65 @@ export default defineComponent({
 
 <template>
   <main>
+    <a-row>
+      <a-col :span="6"
+        ><a-input-search
+          v-model:value="state.searchText"
+          placeholder="Chercher une annonce..."
+          @search="this.getAds()"
+          @change="this.updateSearchText()"
+      /></a-col>
+      <a-col
+        :span="18"
+        style="
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          background-color: red;
+        "
+      >
+        <a-typography-text
+          >Filtres
+          <b>{{ state.filtersEnabled ? "ON" : "OFF" }}</b></a-typography-text
+        >
+        <a-switch
+          v-model:checked="state.filtersEnabled"
+          @change="this.onChangedValues"
+        />
+        <a-checkbox-group
+          v-model:value="state.checkedList"
+          :options="plainOptions"
+          @change="this.onChangedValues"
+        />
+        <a-space>
+          <a-select
+            ref="select"
+            style="width: 120px"
+            @change="this.handleChangeSelectType"
+            :value="state.typeOptions"
+          >
+            <a-select-option value="all">Tous</a-select-option>
+            <a-select-option value="apartment">Appartements</a-select-option>
+            <a-select-option value="house">Maisons</a-select-option>
+          </a-select>
+        </a-space>
+        <a-space>
+          <a-select
+            ref="select"
+            style="width: 120px"
+            @change="this.handleChangeSelectClassification"
+            :value="state.classification"
+          >
+            <a-select-option value="all">Tous</a-select-option>
+            <a-select-option value="excellent">Excellent</a-select-option>
+            <a-select-option value="good">Bon</a-select-option>
+            <a-select-option value="average">Moyen</a-select-option>
+            <a-select-option value="bad">Mauvais</a-select-option>
+            <a-select-option value="insalubrious">Insalubre</a-select-option>
+          </a-select>
+        </a-space>
+      </a-col>
+    </a-row>
     <div class="adsContainer">
       <div class="adsBox" v-for="ad in state.ads" :key="ad.id">
         <a-card hoverable style="width: 400px" bodyStyle="padding: 20px">
@@ -160,7 +318,7 @@ export default defineComponent({
         width: 100%;
       "
     >
-      <div class="fixed bottom-0 pb-3">
+      <div class="fixed bottom-0 pb-3" v-if="!state.filtersEnabled">
         <a-pagination
           v-model:current="state.page"
           :total="state.totalItems"
