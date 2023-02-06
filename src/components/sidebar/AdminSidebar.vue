36d9fffcfc936b0c7914946577897efe1acce510
Sulyk <sylvain.boudacher@gmail.com>
2023-02-06 22:27:15 +0100

    add KPI for fun

--- src/components/sidebar/AdminSidebar.vue
+++ src/components/sidebar/AdminSidebar.vue
@@ -42,9 +42,13 @@
             <span class="flex-1 ml-3 whitespace-nowrap">Utilisateurs</span>
           </a>
         </li>
+
         <li>
-          <a
-            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+          <button
+            type="button"
+            class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+            aria-controls="dropdown-example"
+            data-collapse-toggle="dropdown-example"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -57,8 +61,41 @@
                 d="M19 12h3L12 3 2 12h3v8h2v-2h10v2h2v-8ZM7.21 10h9.58l.21.19V12H7v-1.81l.21-.19Zm7.36-2H9.43L12 5.69 14.57 8ZM7 16v-2h10v2H7Z"
               ></path>
             </svg>
-            <span class="flex-1 ml-3 whitespace-nowrap">Appartement</span>
-          </a>
+            <span
+              class="flex-1 ml-3 text-left whitespace-nowrap"
+              sidebar-toggle-item
+              >Appartement</span
+            >
+            <svg
+              sidebar-toggle-item
+              class="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <ul id="dropdown-example" class="hidden py-2 space-y-2">
+            <li>
+              <a
+                href="#"
+                class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >Tout les appartement</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >En attente de validation</a
+              >
+            </li>
+          </ul>
         </li>
 
         <li>
@@ -90,5 +127,6 @@
   height: 100vh;
   position: fixed;
   margin-left: 0;
+  z-index: 100;
 }
 </style>
--- src/views/admin/HomeAdminView.vue
+++ src/views/admin/HomeAdminView.vue
@@ -1,8 +1,69 @@
-<script setup></script>
+<script setup>
+import { onMounted, reactive } from "vue";
+import { client } from "../../services";
+
+const state = reactive({
+  apartments: [],
+  users: [],
+});
+
+const getAllApartements = async () => {
+  client
+    .get("/real_estate_ads?pagination=false")
+    .then((res) => {
+      const data = res.data;
+      state.apartments = data["hydra:member"];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getUsers = async () => {
+  client
+    .get("/users?pagination=false")
+    .then((res) => {
+      const data = res.data;
+      state.users = data["hydra:member"];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+onMounted(() => {
+  getAllApartements();
+  getUsers();
+});
+</script>
 
 <template>
   <div class="p-4 sm:ml-64">
-    <p>pouet</p>
+    <div
+      class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"
+    >
+      <div class="grid grid-cols-2 gap-4 mb-4">
+        <div
+          class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+        >
+          <p class="text-2xl text-gray-400 dark:text-gray-500">
+            Nombre d'appartement : {{ state.apartments.length }}
+          </p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+        >
+          <p class="text-2xl text-gray-400 dark:text-gray-500">
+            Nombre d'utilisateur : {{ state.users.length }}
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800"
+      >
+        <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+      </div>
+    </div>
   </div>
 </template>
 
