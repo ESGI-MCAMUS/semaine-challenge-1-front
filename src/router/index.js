2c4b971e03e684cfcf90e175ee47492e0de5accc
Sulyk <sylvain.boudacher@gmail.com>
2023-02-07 14:45:36 +0100

    add protection by role to Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { token } from "../utils/localStorage";
 import HomeAdminView from "../views/admin/HomeAdminView.vue";
 import ConfirmEmail from "../views/ConfirmEmailView.vue";
 import HomeView from "../views/HomeView.vue";
@@ -9,6 +10,38 @@ import ProfileView from "../views/ProfileView.vue";
 import RealEstateAdsView from "../views/RealEstateAdsView.vue";
 import RegisterView from "../views/RegisterView.vue";
 import ResetPassword from "../views/ResetPasswordView.vue";
+
+const ifAuthenticated = (to, from, next) => {
+  if (token.value.token) {
+    next();
+    return;
+  }
+  next("/login");
+};
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!token.value.token) {
+    next();
+    return;
+  }
+  next("/");
+};
+
+const ifIsAdmin = (to, from, next) => {
+  if (token.value.token && token.value.role.includes("ROLE_ADMIN")) {
+    next();
+    return;
+  }
+  router.push("/login");
+};
+
+const ifIsUser = (to, from, next) => {
+  if (token.value.token && token.value.role.includes("ROLE_USER")) {
+    next();
+    return;
+  }
+  router.push("/login");
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,21 +55,25 @@ const router = createRouter({
       path: "/register",
       name: "register",
       component: RegisterView,
+      beforeEnter: ifNotAuthenticated,
     },
     {
       path: "/login",
       name: "login",
       component: LoginView,
+      beforeEnter: ifNotAuthenticated,
     },
     {
       path: "/profile",
       name: "profile",
       component: ProfileView,
+      beforeEnter: ifAuthenticated,
     },
     {
       path: "/likes",
       name: "likes",
       component: LikesView,
+      beforeEnter: ifAuthenticated && ifIsUser,
     },
     {
       path: "/confirm/:token",
@@ -62,6 +99,17 @@ const router = createRouter({
       path: "/admin",
       name: "admin_home",
       component: HomeAdminView,
+      beforeEnter: ifIsAdmin,
+    },
+    {
+      path: "/admin/apartment/all",
+      name: "admin_real_estate_ads_all",
+      beforeEnter: ifIsAdmin,
+    },
+    {
+      path: "/admin/apartment/waiting",
+      name: "admin_real_estate_ads_waiting",
+      beforeEnter: ifIsAdmin,
     },
   ],
 });
