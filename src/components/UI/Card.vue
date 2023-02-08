23d22722506062ecffddbfbc58fd59cd63af6a5f
Romain Pierucci <pierucci.romain@gmail.com>
2023-02-08 11:49:40 +0100

    add: date-fns + minimal profile page

--- src/components/UI/Card.vue
+++ src/components/UI/Card.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="bg-white border border-gray-200 rounded-lg shadow-md p-4">
+  <div class="bg-white border border-gray-200 rounded-lg shadow-sm p-4">
     <slot />
   </div>
 </template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,70 +1,103 @@
 <script setup>
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
+import { onMounted, reactive, ref } from "vue";
 import Button from "../components/UI/Button.vue";
 import Card from "../components/UI/Card.vue";
 import Heading from "../components/UI/Heading.vue";
 import { client } from "../services";
-import { useGlobalStore } from "../stores/globalStore";
 import { token } from "../utils/localStorage";
-const store = useGlobalStore();
 
-store.updateName("Tartipouette");
-
-const onUpdateName = () => {
-  store.updateName("OLEL");
-};
+let isLoading = ref(true);
+let user = reactive({});
 
 const getuser = async () => {
   const userId = token.value.id;
-  console.log("userId", userId);
 
   try {
     const res = await client.get(`/users/${userId}`);
-    console.log("res", res);
+    return res;
   } catch (error) {
-    console.log("error", error);
+    return undefined;
   }
 };
+
+onMounted(() => {
+  getuser().then((res) => {
+    user = {
+      ...res.data,
+      birthdate: format(new Date(res.data.birthdate), "d MMMM yyyy", {
+        locale: fr,
+      }),
+    };
+    isLoading.value = false;
+  });
+});
 </script>
 
 <template>
-  <div class="flex">
-    <div
-      class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md"
-    >
-      <div class="flex justify-end px-4 pt-4"></div>
-      <div class="flex flex-col items-center pb-10">
-        <img
-          class="w-24 h-24 mb-3 rounded-full shadow-lg"
-          src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
-          alt="Bonnie image"
-        />
-        <h5 class="mb-1 text-xl font-medium text-gray-900">
-          {{ store.name }}
-        </h5>
-        <span class="text-sm text-gray-500">ROLE</span>
-        <div class="flex mt-4 space-x-3 md:mt-6"></div>
+  <div v-if="isLoading"></div>
+  <div v-else>
+    <div class="flex">
+      <div
+        class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm"
+      >
+        <div class="flex flex-col items-center mt-3">
+          <img
+            class="w-24 h-24 mb-3 rounded-full shadow-lg"
+            src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
+            alt="Profile picture"
+          />
+          <h5 class="mb-1 text-xl font-medium text-gray-900">
+            {{ user.firstname }} {{ user.lastname }}
+          </h5>
+          <span class="text-sm text-gray-500">ROLE</span>
+        </div>
       </div>
-    </div>
-    <Card class="w-[100%] ml-4">
-      <div class="flex flex-col">
-        <Heading>Mes derniers paiements</Heading>
+      <Card class="w-[100%] ml-4">
+        <div class="flex justify-between">
+          <Heading>Mon profil</Heading>
+          <Button> Modifier </Button>
+        </div>
 
-        <Button>Some action</Button>
-        <Button @click="onUpdateName">Change le nom</Button>
-        <Button @click="getuser">Get user console</Button>
-      </div>
-    </Card>
-  </div>
-  <Card class="w-[100%] h-80 mt-4">
-    <Heading>Mes biens</Heading>
-  </Card>
-  <div class="flex">
+        <div class="flex">
+          <div class="flex flex-col mt-4 w-1/2">
+            <div class="flex flex-col">
+              <span class="text-sm text-gray-500">Nom</span>
+              <span class="text-sm text-gray-900">{{ user.lastname }}</span>
+            </div>
+            <div class="flex flex-col mt-4">
+              <span class="text-sm text-gray-500">Prénom</span>
+              <span class="text-sm text-gray-900">{{ user.firstname }}</span>
+            </div>
+          </div>
+          <div class="flex flex-col mt-4">
+            <div class="flex flex-col">
+              <span class="text-sm text-gray-500">Date de naissance</span>
+              <span class="text-sm text-gray-900">{{ user.birthdate }}</span>
+            </div>
+            <div class="flex flex-col mt-4">
+              <span class="text-sm text-gray-500">Email</span>
+              <span class="text-sm text-gray-900">{{ user.email }}</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
     <Card class="w-[100%] h-80 mt-4">
-      <Heading>Mes derniers paiements</Heading>
+      <Heading>Mes biens</Heading>
+      <div>Vous ne posséder aucun bien pour le moment</div>
     </Card>
-    <Card class="w-[100%] h-80 mt-4 ml-4">
-      <Heading>Mes derniers paiements</Heading>
-    </Card>
+    <div class="flex">
+      <Card class="w-[100%] h-80 mt-4">
+        <Heading>Mes derniers paiements</Heading>
+        <div>Vous n'avez procécés à aucuns paiements pour le moment</div>
+      </Card>
+      <Card class="w-[100%] h-80 mt-4 ml-4">
+        <Heading>Mes derniers messages</Heading>
+        <div>Vous n'avez reçus aucun messages pour le moment</div>
+      </Card>
+    </div>
   </div>
 </template>
 
